85017157ea64ea636f31817f6775f4a1433c0688
Spencer Carli <spencercarli@gmail.com>
2017-12-08 11:10:38 -0600

    Exercise 7

--- Exercise7/index.js
+++ Exercise7/index.js
@@ -7,19 +7,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  circle: {
+    backgroundColor: 'red',
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
 });
 
 class Challenge7 extends Component {
-  state = {}
+  state = {
+    radius: new Animated.Value(0),
+  }
 
   handlePress = () => {
+    this.state.radius.setValue(0);
+    Animated.timing(this.state.radius, {
+      toValue: 200,
+      duration: 500,
+    }).start();
   }
 
   render() {
+    const opacity = this.state.radius.interpolate({
+      inputRange: [0, 200],
+      outputRange: [1, 0],
+    })
     return (
       <TouchableOpacity style={styles.container} onPress={this.handlePress}>
         <SafeAreaView style={styles.container}>
-
+          <Animated.View
+            style={[
+              styles.circle,
+              {
+                width: this.state.radius,
+                height: this.state.radius,
+                opacity,
+              }
+            ]}
+          />
         </SafeAreaView>
       </TouchableOpacity>
     );
