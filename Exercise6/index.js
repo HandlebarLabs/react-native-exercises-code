ab43bdecb3992a28767720e2d3dce26a155622cc
Spencer Carli <spencercarli@gmail.com>
2017-12-08 11:09:27 -0600

    Exercise 6 solution

--- Exercise6/index.js
+++ Exercise6/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, Text, Animated } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,6 +15,21 @@ const styles = StyleSheet.create({
     height: 300,
     borderRadius: 10,
   },
+  loadingOverlay: {
+    backgroundColor: 'rgba(0,52,89, 1)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    borderRadius: 10,
+  },
+  loadingText: {
+    color: '#fff',
+    fontWeight: '600',
+  },
 });
 
 const imageSource = () => {
@@ -27,6 +42,20 @@ class Exercise6 extends Component {
   state = {
     loading: false,
     imageSource: imageSource(),
+    opacity: new Animated.Value(1),
+  }
+
+  onLoadStart = () => {
+    this.setState({ loading: true });
+  }
+
+  onLoad = () => {
+    Animated.timing(this.state.opacity, {
+      toValue: 0,
+      duration: 500,
+    }).start(() => {
+      this.setState({ loading :false });
+    });
   }
 
   render() {
@@ -36,7 +65,16 @@ class Exercise6 extends Component {
           <Image
             style={styles.image}
             source={this.state.imageSource}
+            onLoadStart={this.onLoadStart}
+            onLoad={this.onLoad}
           />
+          {this.state.loading
+            ?
+              <Animated.View style={[styles.loadingOverlay, { opacity: this.state.opacity }]}>
+                <Text style={styles.loadingText}>Loading...</Text>
+              </Animated.View>
+            : null
+          }
         </View>
       </View>
     );
