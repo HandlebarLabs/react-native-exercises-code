4ff1898c420ef84bb1cd1f3af6685056ab6c64af
Spencer Carli <spencercarli@gmail.com>
2017-12-08 11:13:16 -0600

    Exercise 9 solution

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import App from './Exercise1';
+// import App from './Exercise1';
 // import App from './Exercise2';
 // import App from './Exercise3';
 // import App from './Exercise4';
@@ -6,7 +6,7 @@ import App from './Exercise1';
 // import App from './Exercise6';
 // import App from './Exercise7';
 // import App from './Exercise8';
-// import App from './Exercise9';
+import App from './Exercise9';
 // import App from './Exercise10';
 
 export default App;
--- Exercise9/index.js
+++ Exercise9/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Dimensions } from 'react-native';
+import { View, Image, ScrollView, StyleSheet, Text, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
@@ -32,14 +32,86 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#ffffff'
   },
+  indicatorWrapper: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 50,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  indicator: {
+    width: 10,
+    height: 10,
+    borderWidth: 2,
+    borderColor: '#fff',
+    borderRadius: 5,
+    marginHorizontal: 2,
+  },
+  indicatorActive: {
+    backgroundColor: '#fff',
+  },
 });
+
+class Swiper extends Component {
+  state = {
+    activeIndex: 0,
+  }
+
+  handleScroll = (e) => {
+    const { layoutMeasurement, contentOffset } = e.nativeEvent;
+    const activeIndex = Math.floor(contentOffset.x / layoutMeasurement.width);
+    this.setState({ activeIndex })
+  }
+
+  renderIndicators = () => {
+    return (
+      <View style={styles.indicatorWrapper}>
+        {this.props.children.map((value, index) => (
+          <View
+            key={index}
+            style={[
+              styles.indicator,
+              this.state.activeIndex === index ? styles.indicatorActive : {},
+            ]}
+          />
+        ))}
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View>
+        <ScrollView
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onScroll={this.handleScroll}
+          scrollEventThrottle={64}
+        >
+          {this.props.children}
+        </ScrollView>
+        {this.renderIndicators()}
+      </View>
+    );
+  }
+}
 
 class Exercise9 extends Component {
   render() {
     return (
-      <View style={styles.screen1}>
-        <Text style={styles.text}>Screen 1</Text>
-      </View>
+      <Swiper>
+        <View style={styles.screen1}>
+          <Text style={styles.text}>Screen 1</Text>
+        </View>
+        <View style={styles.screen2}>
+          <Text style={styles.text}>Screen 2</Text>
+        </View>
+        <View style={styles.screen3}>
+          <Text style={styles.text}>Screen 3</Text>
+        </View>
+      </Swiper>
     );
   }
 }
