3726810c190fb251a4f9d54e9a9ac649bcb73c0f
Spencer Carli <spencercarli@gmail.com>
2017-12-08 11:05:16 -0600

    Attempt at Exercise 5 solution

--- Exercise5/api.js
+++ Exercise5/api.js
@@ -1,3 +1,20 @@
+import { AsyncStorage, NetInfo } from 'react-native';
+
+const getLocalData = (url) => {
+  return AsyncStorage.getItem(url)
+    .then(data => {
+      if (data !== null) {
+        return JSON.parse(data);
+      } else {
+        return [];
+      }
+    })
+};
+
+const saveData = (url, data) => {
+  AsyncStorage.setItem(url, JSON.stringify(data));
+};
+
 const getRemoteData = (url) => {
   return fetch(url)
     .then(response => response.json())
@@ -5,5 +22,21 @@ const getRemoteData = (url) => {
 
 export const getPeople = () => {
   const url = 'https://swapi.co/api/people';
-  return getRemoteData(url);
+  // TODO: This is broken on iOS https://facebook.github.io/react-native/docs/next/netinfo.html
+  return NetInfo.isConnected.fetch()
+    .then((isConnected) => {
+      if (!isConnected) {
+        return getLocalData(url);
+      }
+
+      return getRemoteData(url);
+    })
+    .then(data => {
+      console.log('data', data);
+      return data;
+    })
+    .then((data) => {
+      saveData(url, data);
+      return data;
+    })
 };
