876f3bb09265195f38bc995ebb8f044a409d6aeb
Spencer Carli <spencercarli@gmail.com>
2017-12-08 11:14:20 -0600

    Exercise 10 solution

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import App from './Exercise1';
+// import App from './Exercise1';
 // import App from './Exercise2';
 // import App from './Exercise3';
 // import App from './Exercise4';
@@ -7,6 +7,6 @@ import App from './Exercise1';
 // import App from './Exercise7';
 // import App from './Exercise8';
 // import App from './Exercise9';
-// import App from './Exercise10';
+import App from './Exercise10';
 
 export default App;
--- Exercise10/index.js
+++ Exercise10/index.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import { View, Button, SafeAreaView, StatusBar } from 'react-native';
 
+class StatusBarDeclarative extends Component {
+  componentWillMount() {
+    this.updateStatusBar(this.props);
+  }
+
+  componentWillUpdate(nextProps) {
+    this.updateStatusBar(nextProps);
+  }
+
+  updateStatusBar = ({ lightBar = true, visible = true }) => {
+    if (lightBar === true) {
+      StatusBar.setBarStyle('light-content');
+    } else if (lightBar === false) {
+      StatusBar.setBarStyle('dark-content');
+    }
+
+    StatusBar.setHidden(!visible)
+  };
+
+  render() {
+    return null;
+  }
+};
+
 class Exercise10 extends Component {
   state = {
     lightStatusBar: true,
@@ -10,6 +34,10 @@ class Exercise10 extends Component {
   render() {
     return (
       <SafeAreaView style={{ backgroundColor: '#878E88', flex: 1 }}>
+        <StatusBarDeclarative
+          visible={this.state.statusBarVisible}
+          lightBar={this.state.lightStatusBar}
+        />
         <Button
           title="Show/Hide Status Bar"
           onPress={() => this.setState({ statusBarVisible: !this.state.statusBarVisible })}
