4e2fcfcf0a71be5b6c644c98b43034423993aa79
Spencer Carli <spencercarli@gmail.com>
2017-12-08 10:50:41 -0600

    Exercise 1 solution

--- Exercise1/index.js
+++ Exercise1/index.js
@@ -2,15 +2,34 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+  container: {
+    backgroundColor: 'gray',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  redBox: {
+    backgroundColor: 'red',
+    width: 200,
+    height: 200,
+    flexDirection: 'row',
+  },
+  blueBox: {
+    backgroundColor: 'blue',
+    flex: 2,
+  },
+  greenBox: {
+    backgroundColor: 'green',
+    flex: 1,
+  },
 });
 
 const Exercise1 = () => {
   return (
-    <View>
-      <View>
-        <View />
-        <View />
+    <View style={styles.container}>
+      <View style={styles.redBox}>
+        <View style={styles.blueBox} />
+        <View style={styles.greenBox} />
       </View>
     </View>
   );
